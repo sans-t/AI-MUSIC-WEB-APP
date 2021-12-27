b948bc157235edf9148f1cfc5dd16f8cf0897caa
sans-t <96711350+sans-t@users.noreply.github.com>
2021-12-27 18:46:26 +0530

    uploaded main.js

--- main.js
+++ main.js
@@ -1,1 +1,22 @@
-// Code will come
+music1 = "";
+music2 = "";
+
+function preload()
+{
+    music1 = loadSong(music.mp3);
+    music2 = loadSong(music2.mp3);
+}
+
+function setup()
+{
+    canvas = createCanvas(600, 500);
+    canvas.center();
+
+    video = createCapture(VIDEO);
+    video.hide();
+}
+
+function draw()
+{
+    image(video, 0, 0, 600, 500);
+}
